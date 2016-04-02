86a8d72df084fa19c6d11b1a3e5e06ffd7681065
Nadeeshani <nadee158@gmail.com>
2016-04-02 17:45:22 +0530

    modified

--- db_backups/db_backup_and_restore_commands.sql
+++ db_backups/db_backup_and_restore_commands.sql
@@ -1,2 +1,2 @@
-mysql -u root -p cbtlsdb < D:/CBTLS_Final_Project_MsC/cbtls/db_backups/latest_database.sql
-mysqldump -u root -p  cbtlsdb > D:/CBTLS_Final_Project_MsC/cbtls/db_backups/latest_database_v1.sql
+mysql -u root -p cbtlsdb < D:/CBTLS_Final_Project_MsC/cbtls/db_backups/latest_database_v2.sql
+mysqldump -u root -p  cbtlsdb > D:/CBTLS_Final_Project_MsC/cbtls/db_backups/latest_database_v2.sql
